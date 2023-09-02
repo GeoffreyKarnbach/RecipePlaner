299a4168b4fc6fc5d1d3f85562c8b1fb775ff53a
Geoffrey Karnbach <geoffrey.karnbach@tuwien.ac.at>
2023-09-02 14:35:55 +0200

    #21 Rezepte aus Listenansicht filtern
    Added Frontend Component for filtering
    Backend Filtering TODO with Custom Repository

--- code/frontend/src/app/components/pages/recipe/recipe-list-view/recipe-list-view.component.ts
+++ code/frontend/src/app/components/pages/recipe/recipe-list-view/recipe-list-view.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { LightRecipeDto } from 'src/app/dtos';
-import { RecipeService } from 'src/app/services';
+import { IngredientDto, LightRecipeDto } from 'src/app/dtos';
+import { IngredientService, RecipeService } from 'src/app/services';
 
 @Component({
   selector: 'app-recipe-list-view',
@@ -10,6 +10,7 @@ import { RecipeService } from 'src/app/services';
 export class RecipeListViewComponent {
   constructor(
     private recipeService: RecipeService,
+    private ingredientService: IngredientService
   ) { }
 
   currentPage: number = 0;
@@ -20,8 +21,44 @@ export class RecipeListViewComponent {
   resultCount: number = 0;
   recipes: LightRecipeDto[] = [];
 
+  tags: string[] = [''];
+  categories: string[] = [""];
+
+  selectedTag: string = null;
+  currentIngredientName: string = '';
+
+  tagsDtos: string[] = [];
+  ingredientDtos: IngredientDto[] = [];
+
+  ingredients: IngredientDto[] = [];
+  ingredientHashMap: Map<number, IngredientDto>;
 
   ngOnInit(): void {
+
+    this.recipeService.getAllRecipeTags().subscribe(
+      (tags) => {
+        this.tags = tags;
+        this.tags.sort();
+
+      }
+    );
+
+    this.recipeService.getAllRecipeCategories().subscribe(
+      (categories) => {
+        this.categories = categories.map((category) => category.name);
+      }
+    );
+
+    this.ingredientService.getAll().subscribe(
+      (ingredients) => {
+        this.ingredients = ingredients;
+        this.ingredientHashMap = new Map<number, IngredientDto>();
+        for (let ingredient of this.ingredients) {
+          this.ingredientHashMap.set(ingredient.id, ingredient);
+        }
+      }
+    );
+
     this.refreshData();
   }
 
@@ -54,5 +91,52 @@ export class RecipeListViewComponent {
     }
 
     this.refreshData();
+  }
+
+  searchFilteredPreparation() {
+    console.log("searchFilteredPreparation");
+  }
+
+  onTagAdded(): void {
+    if (this.selectedTag) {
+      this.tags = this.tags.filter(tag => tag !== this.selectedTag);
+      this.tagsDtos.push(this.selectedTag);
+      this.selectedTag = null;
+      this.tagsDtos.sort();
+    }
+  }
+
+  onIngredientAdded(): void {
+    if (this.currentIngredientName === '') {
+      return;
+    }
+
+    const ingredientId = this.getIngredientIdByName(this.currentIngredientName);
+    this.ingredientDtos.push(this.ingredientHashMap.get(ingredientId));
+    this.currentIngredientName = '';
+
+    this.ingredients = this.ingredients.filter((ingredient) => ingredient.id !== ingredientId);
+  }
+
+  deleteTag($event: number) {
+    const tagID = $event - 1;
+
+    this.tags.push(this.tagsDtos.splice(tagID, 1)[0]);
+    this.tagsDtos.sort();
+    this.tags.sort();
+  }
+
+  deleteIngredient($event: number) {
+    const ingredientID = $event;
+
+    this.ingredients.push(this.ingredientDtos.splice(ingredientID, 1)[0]);
+  }
+
+  getIngredientImageById(id: number): string {
+    return this.ingredientHashMap.get(id).imageSource;
+  }
+
+  getIngredientIdByName(name: string): number {
+    return this.ingredients.find((ingredient) => ingredient.name === name).id;
   }
 }
